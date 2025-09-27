b798477947142de7b1e0985fc9729f3d5cb8415e
WinnardArthur <winnardarthur7@gmail.com>
2025-09-27 09:57:41 +0000

    ai-jobs

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,16 +1,20 @@
+import { Agent, createAgent, gemini} from "@inngest/agent-kit";
+
 import { inngest } from './client';
 
 export const helloWorld = inngest.createFunction({
     id: "hello-world"
-}, { event: "test/hello-world" }, async function ({ event, step }) {
+}, { event: "test/hello-world" }, async function ({ event }) {
+
+    const codeAgent = createAgent({
+        name: "codeAgent",
+        system: "You are an expert next.js developer. You write readable, maintanable code. You write simple Next.js & React snippets",
+        model: gemini({model: 'gemini-2.5-pro'})
+    })
+
+    const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
     // Download step
-    await step.sleep("wait-a-moment", "30s");
+    // await step.sleep("wait-a-moment", "5s");
 
-    // Transcript step
-    await step.sleep("wait-a-moment", "10s");
-
-    // Summary step
-    await step.sleep("wait-a-moment", "5s");
-
-    return { message: `Hello ${event.data.email}` }
+    return { message: `Hello ${event.data.value}`, output }
 })
