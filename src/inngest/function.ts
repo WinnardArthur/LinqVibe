1bf824e5ba74fa08b5b387a4a9b6733e2175cfcc
WinnardArthur <winnardarthur7@gmail.com>
2025-09-29 05:51:24 +0000

    e2b-sandboxes

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,20 +1,34 @@
-import { Agent, createAgent, gemini} from "@inngest/agent-kit";
+import { Agent, createAgent, gemini } from "@inngest/agent-kit";
+import { Sandbox } from '@e2b/code-interpreter';
 
 import { inngest } from './client';
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction({
     id: "hello-world"
-}, { event: "test/hello-world" }, async function ({ event }) {
+}, { event: "test/hello-world" }, async function ({ event, step }) {
+
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+        const sandbox = await Sandbox.create("linqvibe-nextjs-test-2");
+
+        return sandbox.sandboxId;
+    })
 
     const codeAgent = createAgent({
         name: "codeAgent",
         system: "You are an expert next.js developer. You write readable, maintanable code. You write simple Next.js & React snippets",
-        model: gemini({model: 'gemini-2.5-pro'})
+        model: gemini({ model: 'gemini-2.5-pro' })
     })
 
     const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
-    // Download step
-    // await step.sleep("wait-a-moment", "5s");
 
-    return { message: `Hello ${event.data.value}`, output }
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+        const sandbox = await getSandbox(sandboxId);
+
+        const host = sandbox.getHost(3000);
+
+        return `https://${host}`;
+    })
+
+    return { sandboxUrl, output }
 })
