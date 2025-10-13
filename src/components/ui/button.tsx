8fa9ff30c37b8ddab3bca78b8b8f6c21e8da92aa
WinnardArthur <winnardarthur7@gmail.com>
2025-10-13 05:00:09 +0000

    apply new theme

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        tertiary: "bg-primary/25 dark:bg-primary/30 text-primary shadow-xs hover:bg-primary/20 dark:hover:bg-primary/25"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
